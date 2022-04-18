ee651825d787dd7dfa7ce3ee30edece407719436
TeiHek <lerandy1235@gmail.com>
2022-04-18 09:18:33 -0700

    main.js comment

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ Randy Le
 4/18/2022
 Rocket Patrol Mods
 Points Breakdown (and time estimate), In order of addition
-10 - Display timer                            (30 mins)
+10 - Display timer                            (1 mins)
 20 - Adding points to clock for hitting ships (1 hr)
 20 - Added smaller ship                       (1 hr programming, 1 hr artwork)
 20 - Mouse controls                           (2 hrs)
 20 - Alternating multiplayer                  (3 hrs)
 5  - FIRE UI text                             (10 mins)
 5  - High score, persists after scene change  (10 mins)
+20 - Art assets (rocket, ships, animations)   (started, but not finished)
 */
 let config = {
     type: Phaser.CANVAS,
